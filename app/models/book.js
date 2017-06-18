344e137a98910caf832ef8529290edef32692680
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-06-18 16:25:51 +0300

    added attributes in the model

--- app/models/book.js
+++ app/models/book.js
@@ -1,5 +1,11 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
 
-export default DS.Model.extend({
 
+export default Model.extend({
+  title: attr('string'),
+  author: attr('string'),
+  year: attr('number'),
+  image: attr('string'),
+  blurb: attr('string')
 });
